2f42dca5dd4075d89adc4626047cc5f32a0d99b4
MinG822 <aestas822@gmail.com>
2019-12-13 14:41:50 +0900

    whitenoise

--- stalkstarserver/wsgi.py
+++ stalkstarserver/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.conf.settings import STATIC_ROOT
+from django.conf import settings
 
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stalkstarserver.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=STATIC_ROOT)
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
