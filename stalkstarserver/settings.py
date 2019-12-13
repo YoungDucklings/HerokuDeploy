9ded9933e9a81d588346f7d59493f0edbe473cc6
MinG822 <aestas822@gmail.com>
2019-12-13 14:09:20 +0900

    static

--- stalkstarserver/settings.py
+++ stalkstarserver/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/admin/'
+STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
 
